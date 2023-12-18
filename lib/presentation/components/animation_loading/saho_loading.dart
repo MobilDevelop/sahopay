6b4fe5dc738ea1414459a4c7e439e4c451cc3ac9
ayubxon <numonovmuhammadayubxon@gmail.com>
2023-12-18 08:51:46 +0500

    yangilandi

--- lib/presentation/components/animation_loading/saho_loading.dart
+++ lib/presentation/components/animation_loading/saho_loading.dart
@@ -1,11 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:gif/gif.dart';
 import 'package:sahopay/presentation/assets/res/app_icons.dart';
-<<<<<<< HEAD
-=======
-import 'package:sahopay/presentation/assets/theme/app_theme.dart';
->>>>>>> 561aeb50106511890c845893305fd196bab1dbb0
+
 
 class SahoLoading extends StatelessWidget {
   const SahoLoading({super.key});
--- lib/presentation/pages/dashboard/components/card_widget.dart
+++ lib/presentation/pages/dashboard/components/card_widget.dart
@@ -3,10 +3,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
-<<<<<<< HEAD
-import 'package:sahopay/domain/common/constants.dart';
-=======
->>>>>>> 561aeb50106511890c845893305fd196bab1dbb0
 import 'package:sahopay/infrastructure/models/dashboard/dashboard_model.dart';
 import 'package:sahopay/presentation/assets/res/app_icons.dart';
 import 'package:sahopay/presentation/assets/res/screen_size.dart';
@@ -16,28 +12,18 @@ import 'package:sahopay/presentation/pages/dashboard/components/background.dart'
 
 class CardWidget extends StatelessWidget {
   const CardWidget({
-<<<<<<< HEAD
     super.key, required this.item, required this.index,
   });
   final DashboardModel item;
   final int index;
-=======
-    super.key, required this.item,
-  });
-  final DashboardModel item;
->>>>>>> 561aeb50106511890c845893305fd196bab1dbb0
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 150.h,
       width: double.maxFinite,
       margin: EdgeInsets.only(bottom: ScreenSize.h20,left: ScreenSize.w12,right: ScreenSize.w12),
-<<<<<<< HEAD
-      padding: EdgeInsets.symmetric(horizontal: ScreenSize.w10),
-=======
->>>>>>> 561aeb50106511890c845893305fd196bab1dbb0
       decoration: BoxDecoration(
-        color: AppTheme.colors.green,
+        color: Colors.green,
         border: Border.all(
           color: AppTheme.colors.black,
           width: .5
@@ -53,103 +39,50 @@ class CardWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(10.r),
         
       ),
-      child: Stack(
-        children: [
-<<<<<<< HEAD
-          ListTile(
-            contentPadding: const EdgeInsets.all(0),
-            leading: Container(
-            height: 40.h,
-            width: 40.h,
-            decoration: BoxDecoration(
-              color: AppTheme.colors.primary,
-              border: Border.all(
+      child: Container(
+         padding: EdgeInsets.only(left: ScreenSize.w14,right: ScreenSize.w12, top: ScreenSize.h10,bottom: ScreenSize.h4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              leading: Container(
+              height: ScreenSize.h32,
+              width: ScreenSize.h32,
+              decoration: BoxDecoration(
                 color: AppTheme.colors.primary,
-                width: 1.5
+                borderRadius: BorderRadius.circular(6.r)
               ),
-              borderRadius: BorderRadius.circular(6.r)
-=======
-          Background(),
-          Container(
-             padding: EdgeInsets.only(left: ScreenSize.w14,right: ScreenSize.w12, top: ScreenSize.h10,bottom: ScreenSize.h4),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ListTile(
-                  contentPadding: const EdgeInsets.all(0),
-                  leading: Container(
-                  height: ScreenSize.h32,
-                  width: ScreenSize.h32,
-                  decoration: BoxDecoration(
-                    color: AppTheme.colors.primary,
-                    borderRadius: BorderRadius.circular(6.r)
-                  ),
-                 // child: Image.network(AppContatants.imageUrl+item.logoUrl,color: AppTheme.colors.white,)
-                ),
-                title: Text(tr('dashboard.number'),style: AppTheme.data.textTheme.titleSmall!.copyWith(color: AppTheme.colors.white12)),
-                subtitle: Text(item.account,style: AppTheme.data.textTheme.labelSmall!.copyWith(color: AppTheme.colors.white12)),
-                ),
-                Gap(ScreenSize.h6),
-                Text(tr('dashboard.balance'),style: AppTheme.data.textTheme.titleSmall!.copyWith(color: AppTheme.colors.white12)),
-                Text("${item.currensyName} ${item.balance}",style: AppTheme.data.textTheme.headlineMedium!.copyWith(color: AppTheme.colors.white12)),
-                Gap(ScreenSize.h6),
-                DottedLine(dashColor: AppTheme.colors.white12),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                   TextButtonX(onPressed: (){}, text: tr('home.transfer'),leftIcon:AppIcons.send,textColor: AppTheme.colors.white12),
-                   Container(
-                    width: 1,
-                    height: ScreenSize.h10,
-                    color: AppTheme.colors.white12,
-                   ),
-                   TextButtonX(onPressed: (){}, text: tr('home.deposit'),leftIcon:AppIcons.money,textColor: AppTheme.colors.white12),
-                     Container(
-                    width: 1,
-                    height: ScreenSize.h10,
-                    color: AppTheme.colors.white12,
-                   ),
-                   TextButtonX(onPressed: (){}, text: tr('home.withdraw'),leftIcon:AppIcons.withdraw,textColor: AppTheme.colors.white12),
-                  ],
-                )
-              ],
->>>>>>> 561aeb50106511890c845893305fd196bab1dbb0
+             // child: Image.network(AppContatants.imageUrl+item.logoUrl,color: AppTheme.colors.white,)
             ),
-            child: Image.network(AppContatants.imageUrl+item.logoUrl,
-            color: AppTheme.colors.white,
-            errorBuilder: (context, error, stackTrace) => Icon(Icons.card_giftcard_outlined,color: AppTheme.colors.white,),)
-          ),
-<<<<<<< HEAD
-          title: Text(tr('dashboard.number'),style: AppTheme.data.textTheme.titleSmall!.copyWith(color: AppTheme.colors.white)),
-          subtitle: Text(item.account,style: AppTheme.data.textTheme.labelSmall!.copyWith(color: AppTheme.colors.white)),
-          ),
-          Gap(ScreenSize.h6),
-          Text(tr('dashboard.balance'),style: AppTheme.data.textTheme.titleSmall!.copyWith(color: AppTheme.colors.white)),
-          Text("${item.currensyName} ${item.balance}",style: AppTheme.data.textTheme.headlineMedium!.copyWith(color: AppTheme.colors.white)),
-          Gap(ScreenSize.h8),
-          DottedLine(dashColor: AppTheme.colors.white),
-           Gap(ScreenSize.h8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-             TextButtonX(onPressed: (){}, text: tr('home.transfer'),leftIcon:AppIcons.send,textColor: AppTheme.colors.white),
-             Container(
-              width: 1,
-              height: ScreenSize.h10,
-              color: AppTheme.colors.white,
-             ),
-             TextButtonX(onPressed: (){}, text: tr('home.deposit'),leftIcon:AppIcons.money,textColor: AppTheme.colors.white),
+            title: Text(tr('dashboard.number'),style: AppTheme.data.textTheme.titleSmall!.copyWith(color: AppTheme.colors.white12)),
+            subtitle: Text(item.account,style: AppTheme.data.textTheme.labelSmall!.copyWith(color: AppTheme.colors.white12)),
+            ),
+            Gap(ScreenSize.h6),
+            Text(tr('dashboard.balance'),style: AppTheme.data.textTheme.titleSmall!.copyWith(color: AppTheme.colors.white12)),
+            Text("${item.currensyName} ${item.balance}",style: AppTheme.data.textTheme.headlineMedium!.copyWith(color: AppTheme.colors.white12)),
+            Gap(ScreenSize.h6),
+            DottedLine(dashColor: AppTheme.colors.white12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+               TextButtonX(onPressed: (){}, text: tr('home.transfer'),leftIcon:AppIcons.send,textColor: AppTheme.colors.white12),
                Container(
-              width: 1,
-              height: ScreenSize.h10,
-              color: AppTheme.colors.white,
-             ),
-             TextButtonX(onPressed: (){}, text: tr('home.withdraw'),leftIcon:AppIcons.withdraw,textColor: AppTheme.colors.white),
-            ],
-          )
-=======
->>>>>>> 561aeb50106511890c845893305fd196bab1dbb0
-        ],
+                width: 1,
+                height: ScreenSize.h10,
+                color: AppTheme.colors.white12,
+               ),
+               TextButtonX(onPressed: (){}, text: tr('home.deposit'),leftIcon:AppIcons.money,textColor: AppTheme.colors.white12),
+                 Container(
+                width: 1,
+                height: ScreenSize.h10,
+                color: AppTheme.colors.white12,
+               ),
+               TextButtonX(onPressed: (){}, text: tr('home.withdraw'),leftIcon:AppIcons.withdraw,textColor: AppTheme.colors.white12),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
